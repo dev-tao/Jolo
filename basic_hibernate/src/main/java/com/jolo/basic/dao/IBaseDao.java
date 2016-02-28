b6cb066a3780a8da737c2dee579eec91bac723ba
devtao <dev.tao@qq.com>
2016-02-28 17:44:56 +0800

    first

---
+++ basic_hibernate/src/main/java/com/jolo/basic/dao/IBaseDao.java
@@ -0,0 +1,133 @@
+package com.jolo.basic.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.jolo.basic.model.Pager;
+
+/**
+ * 公共的Dao处理对象
+ * 包含hql,sql的基本操作
+ * @author devtao
+ *
+ */
+public interface IBaseDao<T> {
+	/**
+	 * 添加对象
+	 * @param t
+	 * @return
+	 */
+	public T add(T t);
+	/**
+	 * 根据ID删除对象
+	 * @param id
+	 */
+	public void delete(int id);
+	/**
+	 * 更新对象
+	 * @param t
+	 */
+	public void update(T t);
+	/**
+	 * 根据ID加载对象
+	 * @param id
+	 * @return
+	 */
+	public T load(int id);
+	
+	/**
+	 * 不分页列表对象
+	 * @param hql 查询对象的hql
+	 * @param args 查询参数
+	 * @return 一组不分页的列表对象
+	 */
+	public List<T>list(String hql,Object[] args);
+	public List<T> list(String hql);
+	public List<T> list(String hql,Object arg);
+	
+	
+	/**
+	 * 基于别名和查询参数的混合列表对象
+	 * @param hql
+	 * @param args
+	 * @param alias 别名对象
+	 * @return
+	 */
+	public List<T> list(String hql,Object[] args,Map<String,Object> alias);
+	public List<T>list(String hql,Map<String,Object> alias);
+	
+	
+	/**
+	 * 分页列表对象
+	 * @param hql 查询对象的hql
+	 * @param args 查询参数
+	 * @return 一组不分页的列表对象
+	 */
+	public Pager<T> find(String hql,Object[] args);
+	public Pager<T> find(String hql);
+	public Pager<T> find(String hql,Object arg);
+	
+	
+	/**
+	 * 基于别名和查询参数的混合列表对象
+	 * @param hql
+	 * @param args
+	 * @param alias 别名对象
+	 * @return
+	 */
+	public Pager<T> find(String hql,Object[] args,Map<String,Object> alias);
+	public Pager<T>find(String hql,Map<String,Object> alias);
+	
+	/**
+	 * 根据hql查询一组对象
+	 * @param hql
+	 * @param args
+	 * @return
+	 */
+	public Object querObject(String hql,Object[] args);
+	public Object querObject(String hql,Object arg);
+	public Object querObject(String hql);
+	
+	/**
+	 * 根据hql更新一组对象
+	 * @param hql
+	 * @param args
+	 * @return
+	 */
+	public Object updateObject(String hql,Object[] args);
+	public Object updateObject(String hql,Object arg);
+	public Object updateObject(String hql);
+	
+	/**
+	 * 根据Sql查询对象，不包含关联对象
+	 * @param sql 查询sql语句
+	 * @param arts 查询条件
+	 * @param clz 查询对象
+	 * @param hasEntity 该对象是否是由hibernate管理的实体对象，如果不是使用setResultTransForm查询
+	 * @return
+	 */
+	public List<T> listBySql(String sql,Object[] args,Class<T> clz,boolean hasEntity);
+	public List<T> listBySql(String sql,Object  arg,Class<T> clz,boolean hasEntity);
+	public List<T> listBySql(String sql,Class<T> clz,boolean hasEntity);
+	public List<T> listBySql(String sql,Object[] args,Map<String,Object> alias ,Class<T> clz,boolean hasEntity);
+	public List<T> listBySql(String sql,Map<String,Object> alias ,Class<T> clz,boolean hasEntity);
+	
+	/**
+	 * 根据Sql查询分页对象，不包含关联对象
+	 * @param sql 查询sql语句
+	 * @param arts 查询条件
+	 * @param clz 查询对象
+	 * @param hasEntity 该对象是否是由hibernate管理的实体对象，如果不是使用setResultTransForm查询
+	 * @return
+	 */
+	public Pager<T> findBySql(String sql,Object[] args,Class<T> clz,boolean hasEntity);
+	public Pager<T> findBySql(String sql,Object  arg,Class<T> clz,boolean hasEntity);
+	public Pager<T> findBySql(String sql,Class<T> clz,boolean hasEntity);
+	public Pager<T> findBySql(String sql,Object[] args,Map<String,Object> alias ,Class<T> clz,boolean hasEntity);
+	public Pager<T> findBySql(String sql,Map<String,Object> alias ,Class<T> clz,boolean hasEntity);
+	
+	
+	
+	
+	
+}
---
+++ basic_hibernate/src/main/java/com/jolo/basic/model/Pager.java
@@ -0,0 +1,56 @@
+package com.jolo.basic.model;
+
+import java.util.List;
+
+/**
+ * 分页类
+ * @author devtao
+ *
+ */
+public class Pager<T> {
+	/**
+	 * 分页大小
+	 */
+	private int size;
+	/**
+	 * 分页起始页
+	 */
+	private int offset;	
+	/**
+	 * 总记录数
+	 */
+	private int total;
+	/**
+	 * 分页的数据
+	 */
+	private List<T> datas;
+	
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int getOffset() {
+		return offset;
+	}
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public List<T> getDatas() {
+		return datas;
+	}
+	public void setDatas(List<T> datas) {
+		this.datas = datas;
+	}
+	
+	
+		
+}
