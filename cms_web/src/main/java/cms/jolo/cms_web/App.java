c57c1db32d4434333490b1098f75469e5a31262f
devtao <dev.tao@qq.com>
2016-03-31 21:53:59 +0800

    web

---
+++ cms_web/src/main/java/cms/jolo/cms_web/App.java
@@ -0,0 +1,13 @@
+package cms.jolo.cms_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
